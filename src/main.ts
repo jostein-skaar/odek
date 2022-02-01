c3cacaa1ed01a6fc891c7f8492e2211e5bd2043f
Jostein Skaar <jostein.skaar@gmail.com>
2022-02-01 17:53:42 +0100

    Starting transistion

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import './style.css';
 
 const logo = document.querySelector<HTMLButtonElement>('.logo-button')!;
-logo.addEventListener('click', (e) => {
-  //startGame();
+logo.addEventListener('click', () => {
+  startGame();
 });
 
 var bg = document.querySelector<HTMLDivElement>('.game-bg')!;
-bg.addEventListener('click', (e) => {
-  // bg.classList.remove('show');
+var h1 = document.querySelector<HTMLDivElement>('.game-bg h1')!;
+
+bg.addEventListener('click', () => {
+  bg.classList.remove('show-bg');
+  h1.classList.remove('show-h1');
 });
 
 function startGame() {
   console.log('klika');
-  bg.classList.add('show');
+  bg.classList.add('show-bg');
+  h1.classList.add('show-h1');
 }
