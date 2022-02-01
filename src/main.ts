00153aa16acfd17d467ddb46922126a351bf197c
Jostein Skaar <jostein.skaar@gmail.com>
2022-02-01 17:16:35 +0100

    Preparing modal

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import './style.css';
+
+const logo = document.querySelector<HTMLButtonElement>('.logo-button')!;
+logo.addEventListener('click', (e) => {
+  //startGame();
+});
+
+var bg = document.querySelector<HTMLDivElement>('.game-bg')!;
+bg.addEventListener('click', (e) => {
+  // bg.classList.remove('show');
+});
+
+function startGame() {
+  console.log('klika');
+  bg.classList.add('show');
+}
