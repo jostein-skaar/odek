c9a7cacdae98b2311c007eb9267666ca103cd0d5
Jostein Skaar <jostein.skaar@gmail.com>
2022-02-03 22:14:15 +0100

    Cleaning up styles and transitions

--- src/main.ts
+++ src/main.ts
@@ -6,44 +6,26 @@ logo.addEventListener('click', () => {
   startGame();
 });
 
-// var bg = document.querySelector<HTMLDivElement>('.game-container')!;
-var headerElement = document.querySelector<HTMLElement>('header')!;
-var mainElement = document.querySelector<HTMLElement>('main')!;
-
-// bg.addEventListener('click', () => {
-//   // bg.classList.remove('show');
-//   // document.body.classList.remove('game-open');
-// });
-
 function startGame() {
-  headerElement.classList.add('hide');
-  mainElement.classList.add('hide');
+  document.body.classList.add('game-open');
+  const gameElement = createGame(document.body);
   setTimeout(() => {
-    headerElement.classList.add('remove');
-    mainElement.classList.add('remove');
-    document.body.classList.add('game-open');
-    const gameElement = createGame(document.body);
-    setTimeout(() => {
-      // Need to wait to the canvas is added to the dom to be able to have transition.
-      gameElement.classList.add('show-canvas');
-      var closeButton = document.createElement('button');
-      closeButton.classList.add('close-button');
-      closeButton.innerText = 'X';
-      document.body.appendChild(closeButton);
-      closeButton.addEventListener('click', () => {
-        document.body.removeChild(closeButton);
-        headerElement.classList.remove('remove', 'hide');
-        mainElement.classList.remove('remove', 'hide');
-        document.body.classList.remove('game-open');
-        gameElement.classList.remove('show-canvas');
-        setTimeout(() => {
-          destroyGame();
-        }, 500);
-      });
-    }, 50);
-  }, 500);
-
-  // bg.classList.add('show');
-  // document.body.classList.add('game-open');
-  // createGame(bg);
+    // Need to wait to the canvas is added to the dom to be able to have transition.
+    // TODO: Listen to an event instead?
+    gameElement.classList.add('show-canvas');
+    var closeButton = document.createElement('button');
+    closeButton.classList.add('close-button');
+    closeButton.innerText = 'X';
+    document.body.appendChild(closeButton);
+    closeButton.addEventListener('click', () => {
+      document.body.removeChild(closeButton);
+      document.body.classList.remove('game-open');
+      gameElement.classList.remove('show-canvas');
+      setTimeout(() => {
+        // Need to wait for the css transitions to be done before cleaning up.
+        // TODO: Prevent destroying if opening game again really fast.
+        destroyGame();
+      }, 500);
+    });
+  }, 50);
 }
